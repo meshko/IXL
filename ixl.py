cae0b8212236e792e8dca47ab65c09b407f2267a
Mikhail Kruk <meshko@gmail.com>
2013-09-29 16:11:45 -0400

    support different areas (they added language arts, woohoo!) specify user agent (seriously?)

--- ixl.py
+++ ixl.py
@@ -9,6 +9,7 @@ import smtplib
 EMAIL_FROM = "meshko@scorch2000.com"
 EMAIL_REPLY_TO = "meshko@gmail.com"
 EMAILS_TO = ["meshko@gmail.com", "boriskruk@gmail.com"]
+#EMAILS_TO = ["meshko@gmail.com"]
 
 def send_email(addr_from, addr_from_reply, addrs_to, subject, msg):
 	server = smtplib.SMTP('localhost')
@@ -19,46 +20,52 @@ def send_email(addr_from, addr_from_reply, addrs_to, subject, msg):
 class IXLTopic:
 	"""topic"""
 	def __init__(self, element):		
+		#print element
 		self.done = False		
 		self.name = element.find("a", attrs={"class":"skill-tree-skill-link"}).findAll("span")[1].text
 		self.url = element.find("a", attrs={"class":"skill-tree-skill-link"})['href']
 		scoreElt = element.find("span", attrs={"class":"skill-tree-skill-score"})
 		if scoreElt: 
+			#print "SCORE ELT: " + socreElt
 			self.score = int(scoreElt.text.strip('()'))
 			imgElt = scoreElt.find("img")
 			if imgElt and imgElt['alt'] == "Medal":
 				self.done = True
 		else:
+			#for e in element.findAll("span"):
+			#	print e
 			self.score = 0		
 
 	def __str__(self):
 		return " ".join([self.name, str(self.score), "www.ixl.com" + self.url])
 
-def produce_report(name, userid, familyid, grade):
+def produce_report(name, userid, familyid, area, grade):
 	http = httplib2.Http()
 	#page = conn.request(u"http://www.ixl.com/","GET")
 
 	#url = 'https://www.ixl.com/signin'   	
-	headers = {'Content-type': 'application/x-www-form-urlencoded'}
-	#response, content = http.request(url, 'POST', headers=headers, body=urllib.urlencode(body))
+	#headers = {'Content-type': 'application/x-www-form-urlencoded'}
+	#response, content = http.request(url, 'POST', headers=headers) #, body=urllib.urlencode(body))
+	#print response
+	#sys.exit(0)
 
-	#print content
 	url = 'https://www.ixl.com/signin/subaccount'
 	body = {'userId': userid, 'familyUserId': familyid}
+	headers = {'Content-type': 'application/x-www-form-urlencoded'}
+	headers['User-Agent'] = 'curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8x zlib/1.2.5]'
 	response, content = http.request(url, 'POST', headers=headers, body=urllib.urlencode(body))
+	#print response
+	#print content
 
 	headers = {'Cookie': response['set-cookie']}
-
-	url = 'http://www.ixl.com/math/%s' % grade   
+	url = 'http://www.ixl.com/%s/%s' % (area, grade)
 	response, content = http.request(url, 'GET', headers=headers)
-
-	#print content
-	#sys.exit(0)
 
 	soup = BeautifulSoup(content)
 	#topics = soup.findAll('li')
 	#print content
 	allTopics = soup.findAll('li', attrs={"class":"skill-tree-skill-node skill-tree-skill-has-icon"})
+	#allTopics = soup.findAll('li', attrs={"class":"skill-tree-skill-node"})
 	#print "found" , len(allTopics), "topics"
 	allTopics = map(lambda el: IXLTopic(el), allTopics)
 
@@ -68,8 +75,10 @@ def produce_report(name, userid, familyid, grade):
 	completedTopics = filter(lambda t: t.done, allTopics)
 	completedTopicsNum = len(completedTopics)
 
-	report = "%s's IXL Report\n\nTotal: %d\nCompleted: %d (%.1f%%)\nNot started: %d\nStarted but incomplete: %d" %\
-	(name, totalTopics, completedTopicsNum, (completedTopicsNum/(totalTopics/100.0)), len(notStartedTopics), len(startedTopics))
+	print "total: %d, not started: %d, started: %d, completed: %d\n" % (totalTopics, len(notStartedTopics), len(startedTopics), completedTopicsNum)
+
+	report = "%s's IXL Report on %s\n\nTotal: %d\nCompleted: %d (%.1f%%)\nNot started: %d\nStarted but incomplete: %d" %\
+	(name, area, totalTopics, completedTopicsNum, (completedTopicsNum/(totalTopics/100.0)), len(notStartedTopics), len(startedTopics))
 
 	if len(startedTopics) > 0:
 		report += "\n\nStarted:\n\n"
@@ -85,8 +94,8 @@ def produce_report(name, userid, familyid, grade):
 			report += "\n"
 
 	#print report
-	send_email(EMAIL_FROM, EMAIL_REPLY_TO, EMAILS_TO, "%s's IXL report" % name, report)
+	send_email(EMAIL_FROM, EMAIL_REPLY_TO, EMAILS_TO, "%s's IXL report on %s" % (name, area), report)
 
-produce_report(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+produce_report(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
 
 
